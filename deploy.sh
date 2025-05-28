66db71193faa0c2ec6b3a6d96ef69124443bac11
Kasprzak, Mikolaj <mikolaj.kasprzak@intel.com>
2025-05-28 08:04:58 -0700

    fix test

--- deploy.sh
+++ deploy.sh
@@ -279,13 +279,13 @@ echo Testing inference performance
 echo '########################################'
 
 # REQUIRED_FPS=${REQUIRED_FPS:-20} # Total, not per camera
-# if ! tests/perf_tests/tc_inference_performance.sh retail \
-#      "sample_data/apriltag-cam1.mp4 sample_data/apriltag-cam2.mp4" 500 \
-#      $REQUIRED_FPS ; then
-#     echo The performance of this computer is insufficient
-#     echo At least ${REQUIRED_FPS}FPS is required for inferencing
-#     exit 1
-# fi
+if ! tests/perf_tests/tc_inference_performance.sh retail \
+     "sample_data/apriltag-cam1.mp4 sample_data/apriltag-cam2.mp4" 500 \
+     $REQUIRED_FPS ; then
+    echo The performance of this computer is insufficient
+    echo At least ${REQUIRED_FPS}FPS is required for inferencing
+    exit 1
+fi
 
 if [ "${SKIP_BRINGUP}" != "1" ] ; then
     echo
